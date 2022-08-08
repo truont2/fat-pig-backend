c5831dbf83d18668cd6ded46ad3021fe16cef2bc
Takara Truong <truont2@gmail.com>
2022-08-07 21:48:39 -0700

    saving stashed updates

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
-      // ssl: {
-      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true),
-      // },
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true),
+      },
       ssl: false
     },
     debug: false,
